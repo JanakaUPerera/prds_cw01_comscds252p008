d3b3aeb58db63ede8f21554dc2c7cfd9a0e2a55e
personal_github <janakaudayangaperera@gmail.com>
2026-02-12 06:48:40 +0530

    feat: Add person base class

---
+++ question1_university_system/person.py
@@ -0,0 +1,44 @@
+"""
+person.py
+This module defines the Person class, 
+which serves as a base class for both Student, Academic and None Academic Staff.
+
+functions:
+- __init__: Initializes a Person object with basic personal attributes.
+- get_info: Returns a formatted string with the person's information.
+- update_contact: Updates the person's contact information.
+"""
+class Person:
+    def __init__(self, 
+                name: str, 
+                person_id: int, 
+                email: str, 
+                phone: str
+                ) -> None:
+        self.name = name
+        self.person_id = person_id
+        self.email = email
+        self.phone = phone
+    
+    """
+    Returns person information as formatted string.
+    """
+    def get_info(self) -> str:
+        return (
+            f"Name: {self.name}\n"
+            f"ID: {self.person_id}\n"
+            f"Email: {self.email}\n"
+            f"Phone: {self.phone}"
+        )
+    
+    """
+    Updates the person's contact information.
+    Parameters:
+    - email (str, optional): The new email address.
+    - phone (str, optional): The new phone number.
+    """
+    def update_contact(self, email: str = None, phone: str = None) -> None:
+        if email:
+            self.email = email
+        if phone:
+            self.phone = phone
